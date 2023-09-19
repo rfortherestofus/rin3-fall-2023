5c5c8ec5816035e64698ee78aebe8a7312e87507
David Keyes <david@rfortherestofus.com>
2023-09-19 14:03:01 -0700

    Start week 2 slides

---
+++ misc/week-2.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+
+penguins <- read_csv(file = "data-raw/penguins.csv")
+
+
+# NA values ---------------------------------------------------------------
+
+penguins
+
+penguins |>
+  mutate(not_actually_na = "NA")
+
+
+# I'm just curious, so feel free to ignore if this is covered later.
+# But, you mentioned that piping sequential summarizes into each other doesn't work to get a single table with multiple columns.
+# Is there a way to do that? I didn't know if mutate would be able to handle taking in a tibble from summarize?
+# I was guessing there must be some other way to combine tibbles?
+# For example, if you were getting the mean bill length from the penguins data,
+# but also wanted to get a mean bill length from some other bird dataset,
+# and have these in the same table side by side
+# (I googled it and it looked like there's a merge() function, but I didn't know if that was the best way to go about it in this case)
+
+penguins_2007 <- penguins |>
+  filter(year == 2007)
+
+penguins_2008 <- penguins |>
+  filter(year == 2008)
+
+penguins_2007 |>
+  summarize(mean_bill_length = mean(bill_length_mm, na.rm = TRUE),
+            max_bill_depth = max(bill_depth_mm, na.rm = TRUE))
