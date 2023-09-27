44104b8e0b6def7f0ed9ae75bb36674bb26a0e18
David Keyes <david@rfortherestofus.com>
2023-09-27 13:32:26 -0700

    Update median-income-viz.R

--- misc/median-income-viz.R
+++ misc/median-income-viz.R
@@ -2,21 +2,20 @@ library(tidyverse)
 library(scales)
 library(obtn)
 
-median_income <- read_csv("data-raw/oregon-median-income-by-county.csv")
+median_income <- read_csv("https://raw.githubusercontent.com/rfortherestofus/rin3-fall-2023/main/data-raw/oregon-median-income-by-county.csv")
 
 median_income_yamhill <- median_income |>
   filter(geography %in% c("Yamhill", "Oregon"))
 
 ggplot(data = median_income_yamhill,
-       aes(x = amount,
-           y = geography,
-           label = amount_formatted,
-           fill = geography)) +
+       aes(x = TODO,
+           y = TODO,
+           label = TODO,
+           fill = TODO)) +
   geom_col(show.legend = FALSE) +
-  geom_text(color = "white",
-            hjust = 1.5) +
+  geom_text(color = TODO,
+            hjust = TODO) +
   scale_fill_manual(values = c(
-    "Yamhill" = "darkgreen",
-    "Oregon" = "grey"
+    TODO
   )) +
-  theme_void()
+  theme_TODO()
