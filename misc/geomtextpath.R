8bf3fb5657d7843ca50221cb00365c35f813d426
David Keyes <david@rfortherestofus.com>
2023-10-05 13:34:03 -0700

    Update geomtextpath.R

--- misc/geomtextpath.R
+++ misc/geomtextpath.R
@@ -1,8 +1,31 @@
 library(tidyverse)
+library(geomtextpath)
+
+# Load data
+penguins <- read_csv("https://raw.githubusercontent.com/rfortherestofus/rin3-fall-2023/main/data-raw/penguins.csv")
+
+# Create a scatter plot and add geomtextpath to show a smooth best fit line with species name
+ggplot(data = penguins, aes(x = bill_length_mm,
+                            y = flipper_length_mm,
+                            group = species,
+                            color = species)) +
+  geom_point() +
+  geomtextpath::geom_labelpath(aes(label = paste0(species)),
+                               stat = "smooth",
+                               alpha = 0.8) +
+  theme(legend.position="none")
+
+
+# Gapminder ---------------------------------------------------------------
+
+library(tidyverse)
 library(gapminder)
 
 gapminder |>
-  filter(country == "Afghanistan") |>
+  filter(continent == "Asia") |>
   ggplot(aes(x = year,
-           y = lifeExp)) +
-  geom_line()
+             y = lifeExp,
+             label = country)) +
+  geom_textline() +
+  # geom_line() +
+  facet_wrap(~country)
