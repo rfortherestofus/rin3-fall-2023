f5b5a34be8fb4222280ae4c0a9568de7748032df
David Keyes <david@rfortherestofus.com>
2023-10-18 16:03:10 -0700

    Add week 6 materials

---
+++ misc/plot-size.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(palmerpenguins)
+library(nflplotR)
+
+penguins_bill_length_by_island <- penguins |>
+  group_by(island) |>
+  summarize(mean_bill_length = mean(bill_length_mm, na.rm = TRUE))
+
+ggplot(data = penguins_bill_length_by_island,
+       aes(x = island,
+           y = mean_bill_length)) +
+  geom_col()
+
+# https://search.r-project.org/CRAN/refmans/nflplotR/html/ggpreview.html
+
+ggpreview()
