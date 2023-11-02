2e2c132be5f7baa0dc42a362e93e0ddcbf1fcbeb
David Keyes <david@rfortherestofus.com>
2023-11-02 09:54:53 -0700

    Update week 8 slides

---
+++ misc/bind-rows-different-cols.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(palmerpenguins)
+
+penguins_2007 <-
+  penguins |>
+  filter(year == 2007)
+
+penguins_2008 <-
+  penguins |>
+  filter(year == 2008)  |>
+  select(-year)
+
+bind_rows(penguins_2007, penguins_2008)
